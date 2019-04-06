f5b33c1ed1a8a474145920578ef77505b2c5e78e
harsh-99 <harshmaheshwari135@gmail.com>
2019-04-06 14:29:06 +0530

    Handled the irregular data input'

--- Power_matlab.m
+++ Power_matlab.m
@@ -5,15 +5,16 @@
 % [1 4 0.01 0.08 0.1 1; 1 3 0.005 0.05 0.15 1; 2 3 0.01 0.06 0.12 1; 2 4 0.005 0.04 0.08 1; 3 4 0.01 0.1 0.1 1]
 
 % Test Case 2
-6
-2
-bus_dat = [1 101 1 0 0 0 0.55 0.13; 2 101 1 0 0 0 0 0; 3 101 1 0 0 0 0.3 0.18; 4 101 1 0 0 0 0.5 0.05; 5 102 1.03 0 0.75 0 0.3 0.1; 6 103 1.02 0 0 0 0 0]
+% 6
+% 2
+% bus_dat = [1 101 1 0 0 0 0.55 0.13; 2 101 1 0 0 0 0 0; 3 101 1 0 0 0 0.3 0.18; 4 101 1 0 0 0 0.5 0.05; 5 102 1.03 0 0.75 0 0.3 0.1; 6 103 1.02 0 0 0 0 0]
+bus_dat = [1 103 1.02 0 0 0 0 0; 2 102 1.03 0 0.75 0 0.3 0.1; 3 101 1 0 0 0 0.55 0.13; 4 101 1 0 0 0 0 0; 5 101 1 0 0 0 0.3 0.18; 6 101 1 0 0 0 0.5 0.05]
 line_dat = [6 2 0.08 0.37 0.28 1; 6 4 0.123 0.518 0.4 1; 5 1 0.723 1.05 0.2 1; 5 3 0.282 0.640 0.3 1; 2 4 0.097 0.407 0.240 1; 2 1 0 0.133 0 1.05; 4 3 0 0.3 0 1.025]
 
 
 nbs = input("Enter number of buses ");
 nmc = input("Enter number of machines ");
-
+	
 % bus_dat = input("Enter bus data");
 % for i = 1:nbs
 %     bus_dat(i, 4) *= pi/180;
@@ -31,7 +32,15 @@ for i = 1:size(line_dat)(1)
     Y_bus(line_dat(i, 2), line_dat(i, 2)) += 1.0/(line_dat(i, 3) + j*line_dat(i, 4)) + j*line_dat(i, 5)/2;
 end
 
-iter = 0
+bus_pq = bus_dat(bus_dat(:, 2) == 101, :);
+bus_pv = bus_dat(bus_dat(:, 2) == 102, :);
+bus_sl = bus_dat(bus_dat(:, 2) == 103, :);
+bus_dat = [bus_pq; bus_pv; bus_sl];
+for i = 1:size(bus_dat)(1)
+	bus_dat(i,1) = i;
+end
+% disp(bus_dat)
+iter = 0;
 while 1
 	iter += 1;
 	bus_pq = bus_dat(bus_dat(:, 2) == 101, :);
@@ -216,8 +225,8 @@ for i = 1:size(line_dat)(1)
 	v1 = bus_dat(line_power(i,1),3)*cos(bus_dat(line_power(i,1),4))+ 1j*(bus_dat(line_power(i,1),3)*sin(bus_dat(line_power(i,1),4)));
 	v2 = bus_dat(line_power(i,2),3)*cos(bus_dat(line_power(i,2),4))+ 1j*(bus_dat(line_power(i,2),3)*sin(bus_dat(line_power(i,2),4)));
 	current = v1-v2/(line_dat(i, 3)+ j*line_dat(i,4));
-	line_power(i, 3) = real((v1-v2)*conj(current));
-	line_power(i, 4) = imag((v1-v2)*conj(current));
+	line_power(i, 3) = ((v1)*conj(current));
+	line_power(i, 4) = ((v2)*conj(current));
 end
 disp(line_power)
 % disp(Y_bus)
